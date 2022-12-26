151f69b04d9288bc23f94af50e33ac186d425a17
tkrkr55 <tkrkr55@naver.com>
2022-12-26 14:45:33 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let List = [
+  "01 . 혜인",
+  "01 . 혜인",
+  "01 . 혜인",
+  "01 . 혜인",
+  "01 . 혜인",
+  "01 . 혜인",
+
+]
+
+let newList =[];
+
+for(let i = 0; i < List.length; i ++){
+  newList.push(List[i].slice(4))
+}
+
